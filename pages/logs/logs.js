dedb89398867d12e4a0ef64f176cddae2550bb0e
孔令政 <konglingzheng@qijiayoudao.com>
2017-05-31 11:20:13 +0800

    init project

---
+++ pages/logs/logs.js
@@ -0,0 +1,16 @@
+//logs.js
+
+import util from '../../utils/util'
+
+Page({
+  data: {
+    logs: []
+  },
+  onLoad: function () {
+    this.setData({
+      logs: (wx.getStorageSync('logs') || []).map(function (log) {
+        return util.formatTime(new Date(log))
+      })
+    })
+  }
+})
